04293fec7454ba83d0bb054c95a1f5474baf7780
Stergios Vogiatzis <tehvog@gmail.com>
2022-03-09 17:14:42 +0200

    Session-06

--- Session-06/Calculator/Class1.cs
+++ Session-06/Calculator/Class1.cs
@@ -1,6 +1,6 @@
 ﻿namespace Calculator
 {
-    public class Calc 
+    public class Calc
     {
         //string input = string.Empty;
         //string operand1 = string.Empty;
@@ -12,9 +12,9 @@
         {
 
         }
-        public double Compute(int count,string operand1,string operand2, string input,char operation)
+        public double Compute(int count, string operand1, string operand2, string input, char operation)
         {
-            
+
             operand2 = input;
             double num1, num2;
             double.TryParse(operand1, out num1);
@@ -23,38 +23,38 @@
             {
                 case 1:
                     return result = num1 + num2;
-                    
-                    
+
+
                 case 2:
                     return result = num1 - num2;
-                    
+
                 case 3:
                     return result = num1 * num2;
-                    
+
                 case 4:
                     if (num2 != 0)
                     {
                         return result = num1 / num2;
-                        
+
                     }
                     else
                     {
                         return double.NaN;
                     }
-                    
+
                 case 5:
                     return result = Math.Pow(num1, num2);
-                    
+
                 case 6:
                     return result = Math.Sqrt(num1);
- 
+
                 default:
                     return double.NaN;
             }
         }
-        public void Clear(int count, string operand1, string operand2,string input)
+        public void Clear(int count, string operand1, string operand2, string input)
         {
-            
+
             input = string.Empty;
             operand1 = string.Empty;
             operand2 = string.Empty;
